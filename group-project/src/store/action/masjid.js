40ce513e7cadaf2135638e083caf6d8738f25b42
andre-fajar-n <afajar@alterra.id>
2020-05-07 16:36:15 +0700

    masjid redux

--- group-project/src/store/action/masjid.js
+++ group-project/src/store/action/masjid.js
@@ -1,5 +1,12 @@
 import axios from "axios"
 
-const apiKey = 
-const baseUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?rankby=distance&type=mosque&key=AIzaSyCCOJg5Yvp9FQ_D19Hr7TP4Lx7PAQaWMDM"
+const apiKey = "AIzaSyCCOJg5Yvp9FQ_D19Hr7TP4Lx7PAQaWMDM"
+const baseUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?rankby=distance&type=mosque&key=${apiKey}`
 
+export const getMasjid = (lon = '-7.973006', lat = '112.6079458') => {
+    return async (dispatch) => {
+        const response = await axios.get(`${baseUrl}&location=${lon},${lat}`)
+        console.warn("cek di action", response.data)
+        dispatch({ type: "GET_MASJID", payload: response.data })
+    }
+}
--- group-project/src/store/index.js
+++ group-project/src/store/index.js
@@ -1,1 +1,13 @@
-a
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk"
+import masjidReducer from "./reducer/masjid";
+
+const rootReducer = combineReducers({
+    masjid: masjidReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+store.subscribe(() => console.warn("cek store", store.getState()))
+
+export default store
