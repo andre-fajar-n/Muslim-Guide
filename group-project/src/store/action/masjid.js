c1cc256ab813e2aa30e593a819adcddedba3a756
andre-fajar-n <afajar@alterra.id>
2020-05-08 13:32:12 +0700

    env

--- group-project/src/store/action/masjid.js
+++ group-project/src/store/action/masjid.js
@@ -4,8 +4,9 @@ export const apiKey = process.env.REACT_APP_API_KEY_GOOGLE_PLACES
 export const baseUrl = `${process.env.REACT_APP_BASE_URL_GOOGLE_PLACES}&key=${apiKey}`
 
 export const getMasjid = (lon, lat) => {
+    console.warn("url", baseUrl)
     return async dispatch => {
-        console.log('sadasdsadsadsadsadsadsad ',lon, lat) 
+        console.log('sadasdsadsadsadsadsadsad ', lon, lat)
         const responseMasjid = await axios.get(`https://cors-anywhere.herokuapp.com/${baseUrl}&location=${lon},${lat}`)
         console.warn("cek becek masjid", responseMasjid.data.results)
         dispatch({ type: "GET_MASJID", payload: responseMasjid.data.results })
